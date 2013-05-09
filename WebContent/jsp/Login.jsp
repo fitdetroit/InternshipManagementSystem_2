2d9440fb4491be137fbe7e4baa4f7b5a4da733ed
damitha kithmal <dkithmalfit@gmail.com>
2013-05-09 21:01:16 +0530

    login fixd

---
+++ WebContent/jsp/Login.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib uri="/struts-tags" prefix="s"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Company Login Page</title>
+</head>
+<body>
+<h2 align="center">Login page</h2>
+
+	<table align="center">
+	<tr><td><h3 align="left">Login page</h3></td> </tr>
+	<tr><td><s:form method="post" action="CheckLogin" >
+
+		<tr><td><s:textfield label="UserName" name="userName" /></td> </tr>
+		<tr><td><s:password label="Password" name="password" /></td> </tr>
+		<tr><td><s:submit label="Login" value="Login"></s:submit></td> </tr>
+
+	</s:form></td></tr>
+	</table>
+	<p align="center"><a href="<s:url action="company/NewCompany"/>">click here to new register company</a></p>
+</body>
+</html>
---
+++ src/ims/actions/CheckSession.java
@@ -0,0 +1,61 @@
+package ims.actions;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CheckSession extends ActionSupport {
+
+	HttpServletRequest request = ServletActionContext.getRequest();
+	HttpSession session2 = request.getSession();
+	String str = (String) session2.getAttribute("userName");
+
+	// this method check allready user logged in or not
+	public String execute() {
+
+		if (str != null) {
+			Map session;
+			session = ActionContext.getContext().getSession();
+			String type = (String) session.get("type");
+			
+			
+			if (type == "admin") 
+			{
+				return "admin";
+
+			} 
+			else if (type == "regiStudent") 
+			{
+				return "regiStudent";
+
+			} 
+			else if (type == "notRegiStudent") {
+				return "notRegiStudent";
+
+			} 
+			else if (type == "allowedCompany") 
+			{
+				return "allowedCompany";
+			} 
+			else if (type == "notAllowedCompany") 
+			{
+				return "notAllowedCompany";
+			} 
+			else 
+			{
+				return SUCCESS;
+			}
+
+		} else {
+			return SUCCESS;
+		}
+
+	}
+
+}
