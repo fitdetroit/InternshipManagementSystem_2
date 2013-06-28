1bd66a0800017bc7afd73f71bfeb1a2ffaaaefa0
damitha kithmal <dkithmalfit@gmail.com>
2013-06-28 22:27:58 +0530

    basic mailing part is done can send simple mail using gmail smtp server but have to develp more this event basic part done

--- src/ims/actions/UpdatingStudentCompany.java
+++ src/ims/actions/UpdatingStudentCompany.java
@@ -1,10 +1,18 @@
 package ims.actions;
 
 import java.util.Map;
+import java.util.Properties;
 
 import ims.business.UpdateStudent;
 import ims.business.UpdateStudentComapny;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -46,6 +54,7 @@ public class UpdatingStudentCompany extends ActionSupport{
 	
 		
 		updateStudentComapny.UpdateStudentStateToInterview(comapnyUserName,getStudentUserName());
+		sendMailToStudentInfromInterview();
 		
 		
 		
@@ -57,6 +66,40 @@ public class UpdatingStudentCompany extends ActionSupport{
 	
 	public String sendMailToStudentInfromInterview()
 	{
+		
+		final String username = "dkithmalfit@gmail.com";
+		final String password = "password";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("dkithmalfit@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("dkithmal10@gmail.com"));
+			message.setSubject("Selected For the interview");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done ");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 		return SUCCESS;
 	}
 	
@@ -76,8 +119,47 @@ public class UpdatingStudentCompany extends ActionSupport{
 		
 		updateStudentComapny.UpdateStudentStateToSelected(comapnyUserName,getStudentUserName());
 		
+		sendMailToStudentInfromSelectedToCompany();
 		
+		return SUCCESS;
 		
+	}
+	
+	public String sendMailToStudentInfromSelectedToCompany()
+	{
+		final String username = "dkithmalfit@gmail.com";
+		final String password = "password";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("dkithmalfit@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("dkithmal10@gmail.com"));
+			message.setSubject("Selected for the Internship ");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done ");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 		return SUCCESS;
 		
 	}
