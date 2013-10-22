69edc44d5041ff227d5a5096afe8deeb079646ff
Chathuri Ilangasinghe <ilchathu001@gmail.com>
2013-10-22 15:01:08 +0530

    Support files created

---
+++ src/ims/actions/SupportAction.java
@@ -0,0 +1,56 @@
+package ims.actions;
+
+import java.security.NoSuchAlgorithmException;
+
+import ims.data.Support;
+import ims.data.User;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SupportAction extends ActionSupport {
+
+	
+	private String supMsgId;
+	private String senderEmail;
+	private String senderName;
+	private String description;
+	
+	Support support = new Support();
+	
+	public String support() throws Exception{
+		
+		support.setSenderEmail(getSenderEmail());
+		support.setSenderName(getSenderName());
+		support.setDescription(getDescription());
+		
+		return SUCCESS;
+		
+	}
+	
+	
+	public String getSupMsgId() {
+		return supMsgId;
+	}
+	public void setSupMsgId(String supMsgId) {
+		this.supMsgId = supMsgId;
+	}
+	public String getSenderEmail() {
+		return senderEmail;
+	}
+	public void setSenderEmail(String senderEmail) {
+		this.senderEmail = senderEmail;
+	}
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
